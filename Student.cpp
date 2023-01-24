70ca859c785a74ec7c72da7a772eb9f83c6b4eb8
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-01-24 14:49:41 -0800

    Finished Getters and Setters

--- Student.cpp
+++ Student.cpp
@@ -4,7 +4,34 @@
  #include "Student.h"
  using namespace std;
 
- void Student::setFirstName(const char* firstNameTemp){
+ void Student::setFirstName(const char* firstNameTemp){//sets first name of a student
    strcpy(firstName, firstNameTemp);
  }
 
+ void Student::setLastName(const char* lastNameTemp){//sets last name of a student
+   strcpy(lastName, lastNameTemp);
+ }
+
+ void Student::setID(int stuIDTemp){//sets id of a student
+   id = stuIDTemp;
+ }
+
+ void Student::setGPA(float gpaTemp){//sets gpa of a student
+   gpa = gpaTemp;
+ }
+
+ char* Student::getFirstName(){//returns first name of a student
+   return firstName;
+ }
+
+ char* Student::getLastName(){//returns last name of a student
+   return lastName;
+ }
+
+ int* Student::getID(){//returns id of a student
+   return &id;
+ }
+
+ float* Student::getGPA(){//returns gpa of a student
+   return &gpa;
+ }
